c21fb4b09fc4b836fdf201c5b80b962e84a04a9d
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-03-03 12:52:48 -0700

    All Enemy types follow new Waypoint stuff
    Enemies no longer keep moving after dying

--- Assets/Scripts/ProperManagers/SearchPath.cs
+++ Assets/Scripts/ProperManagers/SearchPath.cs
@@ -6,16 +6,19 @@ public class SearchPath : MonoBehaviour
 {
     public SearchNode initialNode;
     public SearchNode Node { get; set; }
+    public Enemy enemy { get; private set; }
 
     void Start()
     {
         var go = GameObject.FindGameObjectWithTag("firstCheckpoint");
         Debug.Log(go.name);
-        Node = (initialNode == null) ? go.GetComponent<SearchNode>() : initialNode;       
+        Node = (initialNode == null) ? go.GetComponent<SearchNode>() : initialNode;
+        enemy = GetComponent<Enemy>();
     }
 
     public void Move(Movement movement)
     {
+        if (enemy != null && enemy.IsDead) { GetComponent<KinematicMovement>().Velocity = Vector3.zero; return; }
         if (Node != null)
         {
             movement.MoveTowards(Node.transform.position);
